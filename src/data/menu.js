e0131c9ecf7c4968fd5b4d99dd1835daf474a612
Ayushman-codes <sahuayushman125@gmail.com>
2026-06-06 13:26:23 +0530

    Cakes added

--- src/data/menu.js
+++ src/data/menu.js
@@ -4,12 +4,18 @@
 import customCake1 from "../assets/custom-cake1.jpeg";
 import customCake2 from "../assets/custom-cake2.jpeg";
 import customCake3 from "../assets/custom-cake3.jpeg";
-import customCake4 from "../assets/custom-cake4.jpeg";
+// import customCake4 from "../assets/custom-cake4.jpeg";
+import customCake5 from "../assets/custom-cake5.jpeg";
+import customCake6 from "../assets/custom-cake6.jpeg";
+import customCake7 from "../assets/custom-cake7.jpeg";
+// import customCake8 from "../assets/custom-cake8.jpeg";
+import customCake9 from "../assets/custom-cake9.jpeg";
+import customCake10 from "../assets/custom-cake10.jpeg";
 
 export const menuItems = [
   {
     id: 1,
-    name: "Eggless Chocolate Delight",
+    name: "Eggless brownie 500g",
     price: 1200.0,
     description:
       "Rich, completely eggless chocolate cake with a smooth, velvety finish.",
@@ -17,26 +23,74 @@ export const menuItems = [
   },
   {
     id: 2,
-    name: "Eggless Vanilla Dream",
-    price: 2500.0,
+    name: "Eggless Chocolate Oreo Mousse 500g",
+    price: 750.0,
     description:
       "Soft and fluffy vanilla sponge, perfectly sweet and baked fresh.",
     image: customCake2,
   },
   {
     id: 3,
-    name: "Eggless Butterscotch Crunch",
-    price: 2800.0,
+    name: "Eggless Gulaab Janum Cake 500g",
+    price: 845.0,
     description:
       "Sweet butterscotch layers topped with homemade crunchy praline.",
     image: customCake3,
   },
+  // {
+  //   id: 4,
+  //   name: "red valvet Cake ",
+  //   price: 0.0,
+  //   description:
+  //     "Our premium eggless red velvet with rich cream cheese frosting.",
+  //   image: customCake4,
+  // },
   {
     id: 4,
-    name: "Signature Eggless Red Velvet",
+    name: "Authentic Mango Mousse Cake 500g",
+    price: 900.0,
+    description:
+      "Our premium eggless red velvet with rich cream cheese frosting.",
+    image: customCake5,
+  },
+  {
+    id: 5,
+    name: "Pineapple Cake 500g",
+    price: 375.0,
+    description:
+      "Our premium eggless red velvet with rich cream cheese frosting.",
+    image: customCake6,
+  },
+  {
+    id: 6,
+    name: "Custom Chocolate Mousse Cake 500g",
+    price: 1050.0,
+    description:
+      "Our premium eggless red velvet with rich cream cheese frosting.",
+    image: customCake7,
+  },
+  // {
+  //   id: 8,
+  //   name: "Signature Eggless Red Velvet",
+  //   price: 3500.0,
+  //   description:
+  //     "Our premium eggless red velvet with rich cream cheese frosting.",
+  //   image: customCake8,
+  // },
+  {
+    id: 7,
+    name: "Chocolate Mousse Cake",
     price: 3500.0,
     description:
       "Our premium eggless red velvet with rich cream cheese frosting.",
-    image: customCake4,
+    image: customCake9,
+  },
+  {
+    id: 8,
+    name: "Eggless Mango Cake 500g",
+    price: 379.0,
+    description:
+      "Our premium eggless red velvet with rich cream cheese frosting.",
+    image: customCake10,
   },
 ];
